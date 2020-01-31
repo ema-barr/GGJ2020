70d5057a7008185d476f68d08790271f44659042
Emanuele <emanuele.barracchia@hotmail.it>
2020-02-01 00:10:16 +0100

    First implementation SignalListener

---
+++ Assets/Scripts/Objects/HealthManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthManager : MonoBehaviour
+{
+  public Image shieldHealthBar;
+  public FloatValue shieldHealth;
+
+  // Start is called before the first frame update
+  void Start()
+    {
+    InitHealth();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+  public void InitHealth()
+  {
+    float percCurrHealth = shieldHealth.currentValue / shieldHealth.initialValue;
+    shieldHealthBar.fillAmount = percCurrHealth;
+  }
+
+  public void UpdateHealth()
+  {
+    float percCurrHealth = shieldHealth.currentValue / shieldHealth.initialValue;
+    shieldHealthBar.fillAmount = percCurrHealth;
+  }
+}
