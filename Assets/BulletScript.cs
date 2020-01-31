3ef4211ca3d2c4e5fd97a8f126d67411646bd721
Giovanni <rubinogiov@gmail.com>
2020-02-01 00:06:22 +0100

    Bullet Basic Physic
    I proiettili scappano dal nemico e vanno velooooce veloce veloce

---
+++ Assets/BulletScript.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletScript : MonoBehaviour
+{
+    private Vector3 changeMovement;
+    private Rigidbody2D myRigidbody;
+
+
+    [SerializeField]
+    private float speed = 1f;
+    [SerializeField]
+    private float delayKillBullet = 1f;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myRigidbody = this.GetComponent<Rigidbody2D>();
+
+        Destroy(gameObject, delayKillBullet);
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        GetMovement();
+        if (changeMovement != Vector3.zero)
+        {
+            Move();
+        }
+        
+
+    }
+
+    private void GetMovement()
+    {
+        changeMovement = Vector3.zero;
+        changeMovement.x = 1; // Input.GetAxisRaw("Horizontal");
+        changeMovement.y = 0; // Input.GetAxisRaw("Vertical");
+    }
+
+    private void Move()
+    {
+        changeMovement.Normalize();
+        myRigidbody.MovePosition(
+                transform.position + changeMovement * speed * Time.deltaTime);
+
+        //Play Animation
+        changeMovement.x = Mathf.Round(changeMovement.x);
+        changeMovement.y = Mathf.Round(changeMovement.y);
+        //animator.SetFloat("moveX", changeMovement.x);
+        //animator.SetFloat("moveY", changeMovement.y);
+        //animator.SetBool("isMoving", true);
+    }
+}
---
+++ Assets/Scripts/EnemyScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyScript : MonoBehaviour
+{
+    public GameObject bullet;
+    private Transform EnTrans;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        EnTrans = GetComponent<Transform>();
+        Debug.Log(EnTrans.position);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("space"))
+        {
+            Instantiate(bullet,EnTrans);
+        }
+    }
+}
