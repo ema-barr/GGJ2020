478ac6dd0308abe60c2f6fbff82923bb5db1a1c0
Emanuele <emanuele.barracchia@hotmail.it>
2020-02-02 14:43:32 +0100

    Implemented credits Scene

---
+++ Assets/Scripts/Menu/CreditsMenu.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class CreditsMenu : MonoBehaviour
+{
+  [SerializeField]
+  private string mainMenuScene;
+  // Start is called before the first frame update
+  void Start()
+  {
+
+  }
+
+  // Update is called once per frame
+  void Update()
+  {
+
+  }
+
+
+  public void ToMenu()
+  {
+    SceneManager.LoadScene(mainMenuScene);
+  }
+}
