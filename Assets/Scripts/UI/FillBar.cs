d258c20ff97ae5bf975af339263a6a187f02242f
Emanuele <emanuele.barracchia@hotmail.it>
2020-02-02 09:23:19 +0100

    Implemented health and shield bar

---
+++ Assets/Scripts/UI/FillBar.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FillBar : MonoBehaviour
+{
+  public Image bar;
+  public FloatValue value;
+
+  private void Start()
+  {
+    InitBar();
+  }
+
+  public void InitBar()
+  {
+    float percCurrBar = value.currentValue / value.initialValue;
+    bar.fillAmount = percCurrBar;
+  }
+
+  public void UpdateBar()
+  {
+    float percCurrBar = value.currentValue / value.initialValue;
+    bar.fillAmount = percCurrBar;
+  }
+}
