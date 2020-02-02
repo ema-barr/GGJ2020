8d336a362f9654f554574e8fa48eeb3936b4717f
Giovanni <rubinogiov@gmail.com>
2020-02-02 02:16:30 +0100

    Most of the sprites and 3D art test in

---
+++ Assets/CameraScript.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraScript : MonoBehaviour
+{
+    private Vector3 changeMovement;
+    private Rigidbody2D myRigidbody;
+    [SerializeField]
+    private float speed = 1;
+
+    void Start()
+    {
+        myRigidbody = this.GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        GetMovement();
+        if (changeMovement != Vector3.zero)
+        {
+            Move();
+        }
+    }
+
+        private void GetMovement()
+    {
+        changeMovement = Vector3.zero;
+        changeMovement.y = Input.GetAxisRaw("Vertical");
+    }
+
+    private void Move()
+    {
+        changeMovement.Normalize();
+        myRigidbody.MovePosition(
+                transform.position + changeMovement * speed * Time.deltaTime);
+
+        //Play Animation
+        changeMovement.x = Mathf.Round(changeMovement.x);
+        changeMovement.y = Mathf.Round(changeMovement.y);
+    }
+}
